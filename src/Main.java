967ca3696b875d3f78164c06632bd7170622d859
Andrew Bondarenko <andrew11010700@gmail.com>
2020-10-26 19:33:01 +0200

    Rafactor Commit

--- src/Main.java
+++ src/Main.java
@@ -38,43 +38,6 @@ public class Main extends JPanel {
         }
     }
 
-    void moveUp() throws Exception {
-        direction = 1;
-        while (y != 0) {
-              y--;
-              Thread.sleep(33);
-              repaint();
-        }
-    }
-
-    void moveLeft() throws Exception {
-        direction = 3;
-        while (x != 0) {
-            x--;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-
-
-    void moveDown() throws Exception {
-        direction = 2;
-        while (y != BF_HEIGHT - 64) {
-            y++;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-
-    void moveRight() throws Exception {
-        direction = 4;
-        while (x != BF_WIDTH - 64) {
-            x++;
-            Thread.sleep(33);
-            repaint();
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         Main main = new Main();
         main.runTheGame();
